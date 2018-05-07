aeb825f02b5e063eba1454a98a9aa94bea6e47c6
Josh R. Dunlavy <jdunlavy@gmail.com>
2018-05-07 15:18:29 -0700

    finish exercise preparation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'gw-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector    : "gw-root",
+	templateUrl : "./app.component.html",
+	styleUrls   : ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'gw';
+	public correctAnswer = "electric mucus";
+
+	public isExerciseEnded = false;
+
+	public endTheExercise() {
+		this.isExerciseEnded = true;
+		alert("Done!");
+	}
 }
